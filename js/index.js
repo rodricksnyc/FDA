a2ef2fe1270ab0c68d39ed78d27600742760d174
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-07-23 16:53:58 -0400

    fucking bitch mother fucker

--- js/index.js
+++ js/index.js
@@ -3,38 +3,46 @@ $(document).ready(function () {
 	var flipThis = function() {
 
 		$(".bigCard").toggleClass('flipped')
-}
+	}
 
-$('.flipIt').keypress(
-    flipThis
+	$('.flipIt').keypress(
+		flipThis
 
-        ).click(
-      flipThis
-        );
-
-
+	).click(
+		flipThis
+	);
 
 
-  $("a, button, input, [tabIndex='0']").on("keyup", function (e) {
 
-    var code = (e.keyCode ? e.keyCode : e.which);
-    if (code == 9) {
-      $(this).css('outline', 'dashed 3px #4599ff')
 
-    }
+	$("a, button, input, [tabIndex='0']").on("keyup", function (e) {
 
-  })
-  $("a, button, input, [tabIndex='0']").on('focusout', function() {
-    $(this).css('outline', 'none')
-  })
+		var code = (e.keyCode ? e.keyCode : e.which);
+		if (code == 9) {
+			$(this).css('outline', 'dashed 3px #4599ff')
 
-	var thing = $('.whiteBox').height() + 300
+		}
+
+	})
+	$("a, button, input, [tabIndex='0']").on('focusout', function() {
+		$(this).css('outline', 'none')
+	})
+
+	var thing = $('.whiteBox').height() + 340
 
 	console.log(thing)
 
 	$('.wrapper').css({
 		'height' : thing
 	})
+
+	if ($(document).innerWidth() <= 1078) {
+		var higher = $('.whiteBox').height() + 450
+		$('.wrapper').css({
+			'height' : higher
+		})
+
+	}
 
 
 })
