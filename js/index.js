4bf3ef7cfdd8465e4c616510cc73a0029bd070fa
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-07-23 11:03:42 -0400

    fucking bitch mother fucker

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
 
-	$('.flipIt').on( 'click', function() {
-	$('.bigCard').toggleClass('flipped');
-	$('.front').toggleClass('hidden')
-	});
+	var flipThis = function() {
+
+		$(".bigCard").toggleClass('flipped')
+}
+
+$('.flipIt').keypress(
+    flipThis
+
+        ).click(
+      flipThis
+        );
+
+
+
+
+  $("a, button, input, [tabIndex='0']").on("keyup", function (e) {
+
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 9) {
+      $(this).css('outline', 'dashed 3px #4599ff')
+
+    }
+
+  })
+  $("a, button, input, [tabIndex='0']").on('focusout', function() {
+    $(this).css('outline', 'none')
+  })
+
 
 
 
